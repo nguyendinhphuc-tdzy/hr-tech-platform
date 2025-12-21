1ab19453445b2bec59dd4795957250dd9d45f73b
Phúc Đình Nguyễn <nguyendinhphuc2826@gmail.com>
2025-12-21 20:20:29 +0700

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,9 +1,10 @@
-/* FILE: frontend/src/supabaseClient.js */
+// frontend/src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
+// PHẢI CÓ CHỮ 'export' Ở ĐÂY
 export const supabase = (supabaseUrl && supabaseKey) 
     ? createClient(supabaseUrl, supabaseKey) 
     : null;
