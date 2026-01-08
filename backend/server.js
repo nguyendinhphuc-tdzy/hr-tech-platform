4530aa5e782016d39a805d55d5a98dafeeaa8a54
Phúc Đình Nguyễn <nguyendinhphuc2826@gmail.com>
2026-01-08 17:04:10 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // --- CẤU HÌNH ---
-let ACTIVE_MODEL_NAME = "gemini-1.5-flash"; 
+let ACTIVE_MODEL_NAME = "gemini-2.5-flash"; 
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
